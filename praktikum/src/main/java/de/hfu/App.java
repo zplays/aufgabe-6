4ed000638b954feecfbcc35772fda9e4c251660e
zplays <32310711+zplays@users.noreply.github.com>
2021-12-02 12:57:45 +0100

    Update App.java

--- praktikum/src/main/java/de/hfu/App.java
+++ praktikum/src/main/java/de/hfu/App.java
@@ -1,13 +1,13 @@
 package de.hfu;
-
-/**
- * Hello world!
- *
- */
+import java.util.Locale;
+import java.util.Scanner;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	    public static void main (String [] args){
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("Geben Sie einen text ein");
+	        String eingabe = scan.nextLine();
+	        eingabe = eingabe.toUpperCase();
+	        System.out.println(eingabe);
+	}
 }
