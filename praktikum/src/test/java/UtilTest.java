afbcc16fb62f15e2bfc037c48ad8ade3858c15cb
zplays <32310711+zplays@users.noreply.github.com>
2022-01-25 19:59:12 +0100

    Update UtilTest.java
    weiter

--- praktikum/src/test/java/UtilTest.java
+++ praktikum/src/test/java/UtilTest.java
@@ -1,16 +1,39 @@
-import static org.junit.Assert.*;
-
-
+import de.hfu.Util;
 import org.junit.Test;
-
-import junit.framework.Assert;
+import static org.junit.Assert.*;
 
 public class UtilTest {
 
-	@Test
-	public void test() {
-		Assert.assertEquals(true, Util.istErstesHalbjahr(6));
-		Assert.assertEquals(false, Util.istErstesHalbjahr(8));
-	}
+    @Test
+    public void testistErsteHalbjahr() {
 
+        boolean test1 = Util.istErstesHalbjahr(1);
+        boolean test2 = Util.istErstesHalbjahr(6);
+
+        boolean test3 = Util.istErstesHalbjahr(7);
+        boolean test4 = Util.istErstesHalbjahr(12);
+
+
+        assertTrue(test1);
+        assertTrue(test2);
+
+        assertFalse(test3);
+        assertFalse(test4);
+
+
+        try {
+            boolean test = Util.istErstesHalbjahr(0);
+            assertTrue(false);
+        }
+        catch(IllegalArgumentException e){
+        }
+
+        try{
+            boolean test = Util.istErstesHalbjahr(13);
+
+        }
+        catch(IllegalArgumentException e){
+        }
+
+    }
 }
